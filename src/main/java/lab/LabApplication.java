d803d352361225b29403bc9fb0a439f4f7915acb
Hyeon9mak <jinha3507@gmail.com>
2021-08-19 07:33:57 +0900

    feat: 메시지 및 에러 콘솔 출력

--- src/main/java/lab/LabApplication.java
+++ src/main/java/lab/LabApplication.java
@@ -1,7 +1,11 @@
 package lab;
 
 public class LabApplication {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("로깅 재밌냐?");
+            System.err.println("에러 재밌냐?");
+        }
     }
 }
